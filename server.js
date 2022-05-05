ab9426c1753446839c09a50d1029cd6cba264293
Julian Garner <juliangarner240@gmail.com>
2022-05-05 17:58:51 -0500

    api port change

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const onListening = () => {
     console.debug(" Listening on " + bind);
 };
 
-const port = normalizePort(process.env.PORT || "3000");
+const port = normalizePort(process.env.PORT || "3001");
 app.set("port", port);
 
 const server = http.createServer(app);  
